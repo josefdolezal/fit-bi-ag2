e23a8e094b2fe0797f7fc69dce62ab10e76060a0
Josef Dolezal <pepik.ml@gmail.com>
2017-03-20 21:11:00 +0100

    Create project for assignment one

---
+++ assignment-one/assignment-one/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment-one
+//
+//  Created by Josef Dolezal on 20/03/2017.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
